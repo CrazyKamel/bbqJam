0f42a2c94ce0806d1567228ea4a1899ede71f88f
Nael M <nael.messaoud06@gmail.com>
2024-06-29 18:33:01 +0200

    add particles and fix crash
    fixed crash when sausage is deleted and held

--- src/Minigame/Scripts/Minigame.gd
+++ src/Minigame/Scripts/Minigame.gd
@@ -11,6 +11,9 @@ func _ready():
 func connect_to_pickable(object):
 	object.clicked.connect(_on_pickable_clicked)
 
+func disconnect_from_pickable(object):
+	object.clicked.disconnect(_on_pickable_clicked)
+
 func _on_pickable_clicked(object):
 	if !held_object:
 		object.pickup()
@@ -19,7 +22,8 @@ func _on_pickable_clicked(object):
 func _unhandled_input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if held_object and !event.pressed:
-			held_object.drop(Input.get_last_mouse_velocity())
+			if held_object != null:
+				held_object.drop(Input.get_last_mouse_velocity())
 			held_object = null
 
 func _on_sausage_spawner_pressed():
