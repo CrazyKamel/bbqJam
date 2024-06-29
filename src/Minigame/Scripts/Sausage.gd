daad69cab09735bd5bbac4d8aa92c13bc28f7aed
Nael M <nael.messaoud06@gmail.com>
2024-06-29 12:47:02 +0200

    feat: minigame cooking and sprites

--- src/Minigame/Scripts/Sausage.gd
+++ src/Minigame/Scripts/Sausage.gd
@@ -1,22 +1,55 @@
-extends Node2D
+extends RigidBody2D
 
-var selected = false
-var mouse_offset = Vector2.ZERO
+signal clicked
 
-var temperature = 0
+var held = false
+var cooking = false
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# 0 : Raw, 1 : Cooking, 2 : Cooked, 3 : Burnt
+var state = 0
+
+var time_cooking = 0.0
+
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
 func _process(delta):
-	if selected:
-		followMouse()
+	if cooking:
+		time_cooking += delta
 
-func followMouse():
-	position = get_global_mouse_position() + mouse_offset
+	match state:
+		0:
+			if ( time_cooking >= 0.5 ):
+				state = 1
+				print("Sausage cooking!")
+				$AnimatedSprite2D.animation = "Cooking"
+		1:
+			if ( time_cooking >= 10 ):
+				state = 2
+				print("Sausage cooked!")
+				$AnimatedSprite2D.animation = "Cooked"
+		2:
+			if ( time_cooking >= 20 ):
+				state = 3
+				print("Sausage burnt!")
+				$AnimatedSprite2D.animation = "Burnt"
 
-func _on_area_2d_input_event(viewport, event, shape_idx):
+func _on_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.pressed:
-			mouse_offset = position - get_global_mouse_position()
-			selected = true
-		else: #On mouse release
-			selected = false
+			clicked.emit(self)
+
+func _physics_process(delta):
+	if held:
+		global_transform.origin = get_global_mouse_position()
+		
+func pickup():
+	if held:
+		return
+	freeze = true
+	held = true
+	
+func drop(impulse=Vector2.ZERO):
+	if held:
+		freeze = false
+		apply_central_impulse(impulse)
+		held = false
