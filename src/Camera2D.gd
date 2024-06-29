9bf91d3acdbc664a7d80ff1351a9da40bcffa5c7
Dino <72475325+dinoclier72@users.noreply.github.com>
2024-06-29 10:43:07 +0200

    lerping zoom
    when zooming is smooth as hell

--- src/Camera2D.gd
+++ src/Camera2D.gd
@@ -11,21 +11,29 @@ const ZOOM_POWER = 10
 var original_zoom = Vector2(1, 1)
 var original_position = Vector2()
 
+var tCamera = 0
+var tZoom = 0
+const lerpSpeedCameraIn = 0.8
+const lerpSpeedCameraOut = 0.4
+const lerpSpeedZoomIn = 0.8
+const lerpSpeedZoomOut = 1.6
+
+
 # Define the object to focus on
 @export var target: Node2D
+var target_zoom = Vector2(1, 1)
+var target_position = Vector2()
 
 func _ready():
 	original_zoom = zoom
 	original_position = global_position
+	target_zoom = Vector2(ZOOM_POWER, ZOOM_POWER)
+	target_position = target.global_position
 
 func _input(event):
 	if event is InputEventKey:
 		if event.pressed:
 			if event.keycode == SWITCH_VIEW_KEY:
-				if officeZoomed:
-					zoom_out()
-				else:
-					zoom_in()
 				officeZoomed = !officeZoomed
 
 func zoom_in():
@@ -47,3 +55,21 @@ func focus_on_target():
 	if target:
 		# Calculate the new position to focus on the target
 		global_position = target.global_position
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if(tCamera < 1):
+		if officeZoomed:
+			tCamera += delta * lerpSpeedCameraIn
+	if(tCamera > 0):
+		if !officeZoomed:
+			tCamera -= delta * lerpSpeedCameraOut
+	if(tZoom < 1):
+		if officeZoomed:
+			tZoom += delta * lerpSpeedZoomIn
+	if(tZoom > 0):
+		if !officeZoomed:
+			tZoom -= delta * lerpSpeedZoomOut
+
+	zoom = original_zoom.lerp(target_zoom, tZoom)
+	global_position = original_position.lerp(target_position, tCamera)
