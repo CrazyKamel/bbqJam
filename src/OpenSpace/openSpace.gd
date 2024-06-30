78e97ae6287ad63eaa93fea847f7c253686cb3a1
Nael M <nael.messaoud06@gmail.com>
2024-06-30 16:09:02 +0200

    fix client plate hitbox, add emote, adjust hour hand

--- src/OpenSpace/openSpace.gd
+++ src/OpenSpace/openSpace.gd
@@ -79,3 +79,7 @@ func subscribeCancelQuit():
 func onGameEnd():
 		Global.goto_scene("res://src/backgroundEnd/end.tscn")
 		
+
+func _on_area_2d_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.pressed:
+		swapCamera()
